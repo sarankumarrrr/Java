197ee621025b05f429f8c6df56c5b2ca7d4cb130
sarankumarrrr <sarankumarvelmurugann@gmail.com>
2026-01-29 23:32:49 +0530

    IfElseDemo.java file added

---
+++ IfElseDemo.java
@@ -0,0 +1,15 @@
+class IfElseDemo
+{
+    public static voiid main(String args[])
+    {
+        int num=10;
+        if(num%2==0)
+        {
+            System.out.println("The number is even");
+        }
+        else
+        {
+            System.out.println("The number is odd");
+        }
+    }
+}
